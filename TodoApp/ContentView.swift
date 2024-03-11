108fda6485c764a3a73235d19ad55d78b04c10b5
atsutama2 <atsutama.0713@gmail.com>
2024-03-11 19:38:39 +0900

    initial commit

---
+++ TodoApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by Yuya Fujita on 2024/03/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Yuya Fujita on 2024/03/11.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
